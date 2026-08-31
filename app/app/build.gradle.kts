04104e2c332a2b869844bc9a7b4013e39cc1f4be
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2026-08-31 13:44:53 +0200

    bumped version to 1.8.1

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "com.github.livingwithhippos.unchained"
         minSdk = 27
         targetSdk = 37
-        versionCode = 61
-        versionName = "1.8.0"
+        versionCode = 62
+        versionName = "1.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
